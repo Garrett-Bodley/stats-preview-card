395a40c771d6783f3206736fdd6792994252a950
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-08 16:02:04 -0400

    Import StatsPreviewCard.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import StatsPreviewCard from './StatsPreviewCard';
 
 function App() {
   return (
     <div className="App">
       App
+      <StatsPreviewCard />
     </div>
   );
 }
