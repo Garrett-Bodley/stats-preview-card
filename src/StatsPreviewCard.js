6b5dc365bd0dee363b424d4b5bd1d40e961bb1cc
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-08 16:01:40 -0400

    Stub out html structure of component.

--- src/StatsPreviewCard.js
+++ src/StatsPreviewCard.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const StatsPreviewCard = () => {
   return (
-    <main>
-      
+    <main className="card">
+      <header className="image-header"></header>
+      <article className="card-content">
+        <h1 className="card-title">
+          Get insights that help your business grow.
+        </h1>
+        <p className="card-text">Discover the benefits of data analytics and make better decisions regarding revenue, customer 
+  experience, and overall efficiency.</p>
+        <section className="detail-highlights">
+          <div className="detail-item">10k+ companies</div>
+          <div className="detail-item">314 templates</div>
+          <div className="detail-item">12m+ queries</div>
+        </section>
+      </article>
     </main>
-  )
-}
+  );
+};
 
-export default StatsPreviewCard
+export default StatsPreviewCard;
